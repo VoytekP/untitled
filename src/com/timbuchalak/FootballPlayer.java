3aae2617a1152585d4792e9f3ac8c41e9f8db4ea
Voytek Paterek <wojciech.paterek@gmail.com>
2020-04-05 13:15:46 +0200

    Initial commit

---
+++ src/com/timbuchalak/FootballPlayer.java
@@ -0,0 +1,7 @@
+package com.timbuchalak;
+
+public class FootballPlayer extends Player {
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ src/com/timbuchalak/Main.java
@@ -0,0 +1,24 @@
+package com.timbuchalak;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballPlayerTeam = new Team<>("Chicago Cubs");
+        baseballPlayerTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
+        brokenTeam.addPlayer(beckham);
+
+    }
+}
